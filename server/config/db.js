ffa3355e26c36af9c3f29435286d7325bcd70323
Dechen Dolkar <dechendolkar@MacBookAir.lan>
2026-06-15 22:43:39 +0200

    feat: create database connection

---
+++ server/config/db.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose'
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI)
+        console.log(`Connected to MongoDB: ${conn.connection.host} :)`)
+    } catch (error) {
+        console.error(`Error while connected to MongoDB: ${error.message} :(`)
+        process.exit(1)
+    }
+}
+
+export default connectDB
+
